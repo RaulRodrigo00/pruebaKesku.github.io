e732c4d946bd5d8b8f1c685dc5aa5a1ebe8821b4
Raul Barbosa <rbarbosa@deacero.com>
2022-02-25 12:06:19 -0600

    prueba15

--- script.js
+++ script.js
@@ -6,12 +6,13 @@ const circles = document.querySelectorAll('.kesku-circles');
 const keskuLogo = document.querySelector('#kesku-svg-logo');
 const keskuApiPath = document.querySelector('#kesku-api-path');
 
-const keskuTriang = document.getElementById('keskuTriang');
-keskuTriang.style.visibility='visible';
-
+const parrafo = document.querySelector('.pprueba');
 const offset = document.querySelector('.offset');
+const offsetslice = document.querySelector('.offsetslice');
 
-
+//parrafo.textContent="prueba9";
+const keskuTriang = document.getElementById('keskuTriang');
+keskuTriang.style.visibility='visible'
 window.addEventListener('DOMContentLoaded', function() {
   svgPaths.forEach(svg => {
     const length = svg.getTotalLength();
@@ -31,36 +32,37 @@ window.addEventListener('scroll', function() {
   
     svg.style.strokeDashoffset = length;
 
-    const scrollpercent = ((document.body.scrollTop + document.documentElement.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight));
-
+    var scrollpercent = ((document.body.scrollTop + document.documentElement.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight)) +.1 ;
+    if(scrollpercent === 1.1){
+      scrollpercent = 1;
+    }
+    if(scrollpercent === .1){
+      scrollpercent = 0;
+    }
     //  console.log(scrollpercent)
     const draw = length * scrollpercent;
 
     svg.style.strokeDashoffset = length - draw;
 
+    //offset.textContent=svg.style.strokeDashoffset;
+    //offsetslice.textContent=svg.style.strokeDashoffset.slice(0, -2);
 
-    console.log("prueba14");
     console.log("el dashoffset", svg.style.strokeDashoffset);
-    offset.textContent = svg.style.strokeDashoffset;
     //console.log("el slice es: ", svg.style.strokeDashoffset.slice(0, -2));
 
-    if(svg.style.strokeDashoffset <= 300 || svg.style.strokeDashoffset.slice(0, -2) <= 300) {
+    if(svg.style.strokeDashoffset <= 100 || svg.style.strokeDashoffset.slice(0, -2) <= 100) {
       keskuApiPath.classList.add('show');
-      console.log("los muestra")
     } else {
       keskuApiPath.classList.remove('show');
-
     }
 
-    if(svg.style.strokeDashoffset <= 300 || svg.style.strokeDashoffset.slice(0, -2) <= 300) {
+    if(svg.style.strokeDashoffset <= 150 || svg.style.strokeDashoffset.slice(0, -2) <= 150) {
       keskuLogo.classList.add('animated');
       keskuTriang.classList.add('triangAnimation');
     } else {
       keskuLogo.classList.remove('animated');
       keskuTriang.classList.remove('triangAnimation');
     }
-
-
   });
 
   circles.forEach(circle => {
